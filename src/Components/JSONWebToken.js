feabf803983a140d9519838d6b2db212ef87b67f
SamMathewson98 <sammathewson98@gmail.com>
2023-10-14 07:47:35 -0600

    Updates

--- src/Components/JSONWebToken.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-const secretKey = 'your-secret-key'; // Keep this secret
-
-// Create a JWT on user login
-const token = jwt.sign({ userId: user.id }, secretKey, { expiresIn: '1h' });
-
-// Verify a JWT
-const decodedToken = jwt.verify(token, secretKey);
--- src/Components/gunShop.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './OverallStyleSheet.css';
-
-const GunShop = () => {
-  return <div className='webHeader'><h1>Welcome to Combat Customs</h1></div>;
-};
-
-export default GunShop;
